d63be81c368b31541442befe516f27d5b35f6e8c
itzaeuala <86620679+itzaeuala@users.noreply.github.com>
2021-07-09 13:22:00 -0500

    Se agregó build.gradle.kts para el mejor manejo de las dependencias

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,10 @@
+object Versions {
+    const val kotlinVersion = ""
+    const val coreKtxVersion = "1.6.0"
+    const val appcompatVersion = "1.3.0"
+    const val materialVersion = "1.4.0"
+    const val constraintlayoutVersion = "2.0.4"
+    const val junitVersion = "4.13.2"
+    const val extjunitVersion = "1.1.3"
+    const val espressocoreVersion = "3.4.0"
+}
